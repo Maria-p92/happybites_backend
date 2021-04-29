0ca126e51347e649f845c40d3819252af5e79937
Ainamkoz Bakytkaliyeva <ainamkoz.bakyt@gmail.com>
2021-04-29 18:46:46 +0600

    middleware

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import express from 'express';
 import {signIn, register, getUserInfo} from '../controllers/auth.js';
-import {verifyToken} from '../middlewares/verifyToken.js'
+import verifyToken from '../middlewares/verifyToken.js'
 
 const auth = express.Router();
 
