c1dc91653e012d17f3f10af0f6defb42cf4e14d3
Maria-p92 <mariaparradesign@gmail.com>
2021-05-11 22:23:23 +0200

    everything done

--- controllers/companyRequests.js
+++ controllers/companyRequests.js
@@ -3,7 +3,7 @@ import dbConnection from "../db/mysql.js";
 export const getRequestsAll = async (req, res) => {
   try {
     await dbConnection.query(
-      "SELECT  email, phone, guest_number, event_date, message, price, user_id, service_id, company_id FROM Company_requests JOIN User_profile ON Company_requests.event_id=User_profile.profileUser_id",
+      "SELECT Company_requests.email, Company_requests.phone, Company_requests.guest_number, Company_requests.event_date, Company_requests.message, Company_requests.price, Company_requests.user_id, Company_requests.service_id, Company_requests.company_id FROM Company_requests JOIN User_profile ON Company_requests.profileUser_id=User_profile.profileUser_id",
       function (err, result) {
         if (err) throw err;
         res.json(result);
@@ -16,8 +16,9 @@ export const getRequestsAll = async (req, res) => {
 
 export const getSingleRequestPerUser = async (req, res) => {
   try {
+    console.log(req.params.id)
     await dbConnection.query(
-      "SELECT * FROM Company_requests JOIN User_profile ON Company_requests.event_id=User_profile.profileUser_id WHERE User_profile.profileUser_id=" +
+      "SELECT * FROM Company_requests JOIN User_profile ON Company_requests.profileUser_id=User_profile.profileUser_id WHERE User_profile.profileUser_id=" +
         req.params.id,
       function (err, result) {
         if (err) throw err;
@@ -56,7 +57,6 @@ export const createSingleRequest = async (req, res) => {
         price,
       ],
     ];
-    console.log(req.body);
     try {
       await dbConnection.query(
         "INSERT INTO Company_requests (profileUser_id, company_id, service_id, email, phone, guest_number, event_date, message, price) VALUES ?",
